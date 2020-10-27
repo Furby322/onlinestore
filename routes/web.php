6765d96a85ceb860a809064d76dd7ac59e2000f6
furby322 <miha.hleb322@gmail.com>
2020-10-27 23:54:58 +0300

    Basket sum + count

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,10 @@ Route::get('/', 'MainController@index')->name('index');
 Route::get('/сategories', 'MainController@сategories')->name('categories');
 
 Route::get('/basket', 'BasketController@basket')->name('basket');
-
 Route::get('/basket/place', 'BasketController@basketPlace')->name('basket-place');
-
 Route::post('/basket/add/{id}', 'BasketController@basketAdd')->name('basket-add');
+Route::post('/basket/remove/{id}', 'BasketController@basketRemove')->name('basket-remove');
 
 Route::get('/{category}', 'MainController@сategory')->name('category');
-
 Route::get('/{category}/{product?}', 'MainController@product')->name('product');
 
