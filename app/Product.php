6f05051a4c048738be5a10489033bb27c464391d
furby322 <miha.hleb322@gmail.com>
2021-01-16 02:02:13 +0300

    Storage(Photo products), Validations, Request/Resource Controllers (Products, Categories), REST

--- app/Product.php
+++ app/Product.php
@@ -11,6 +11,10 @@ class Product extends Model
 //        return Category::find($this->category_id);
 //    }
 
+    protected $fillable = [
+        'name', 'code', 'price', 'category_id', 'description', 'image'
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
